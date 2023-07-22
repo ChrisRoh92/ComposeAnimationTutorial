efc1ff9474fedd7d0c8c0eb298bfe702464fda22
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2023-07-22 13:02:59 +0200

    Added CrossFadeAnimation

--- app/src/main/java/de/codingwithchris/composeanimationtutorial/AnimationExamples.kt
+++ app/src/main/java/de/codingwithchris/composeanimationtutorial/AnimationExamples.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,6 +21,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
@@ -114,7 +118,62 @@ fun PulsatingAnimation(modifier: Modifier = Modifier)
 @Composable
 fun CrossFadeAnimation(modifier: Modifier = Modifier)
 {
-    // TODO: Implement!
+    var isToggled by remember { mutableStateOf(false) }
+
+    val fadeInOutAlpha = animateFloatAsState(
+        targetValue = if (isToggled) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000)
+    ).value
+
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { isToggled = !isToggled },
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Text(text = "Toggle")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(1f - fadeInOutAlpha),
+                shape = RoundedCornerShape(16.dp),
+            ) {
+                // Content of the first view
+                Text(
+                    text = "View 1",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+
+            Card(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .alpha(fadeInOutAlpha),
+
+                shape = RoundedCornerShape(16.dp),
+            ) {
+                // Content of the second view
+                Text(
+                    text = "View 2",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
 }
 
 @Composable
