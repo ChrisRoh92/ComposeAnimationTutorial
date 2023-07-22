fcadad972ff84bbb1ff2af48a9041677f041781c
Christoph Rohnert <Christoph.Rohnert@t-online.de>
2023-07-22 12:57:14 +0200

    Added PulsatingButtonAnimation

--- app/src/main/java/de/codingwithchris/composeanimationtutorial/AnimationExamples.kt
+++ app/src/main/java/de/codingwithchris/composeanimationtutorial/AnimationExamples.kt
@@ -1,7 +1,37 @@
 package de.codingwithchris.composeanimationtutorial
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+
 
 // Slide-In Animation Code Snippet
 @Composable
@@ -48,7 +78,37 @@ fun SlideInAnimation(
 @Composable
 fun PulsatingAnimation(modifier: Modifier = Modifier)
 {
-    // TODO: Implement!
+    var isPulsating by remember { mutableStateOf(false) }
+
+    val pulseAnimation = animateFloatAsState(
+        targetValue = if (isPulsating) 1.2f else 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 1000
+                0.7f at 500
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    ).value
+
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { isPulsating = !isPulsating },
+            shape = CircleShape,
+            modifier = Modifier
+                .size(100.dp)
+                .graphicsLayer(scaleX = pulseAnimation, scaleY = pulseAnimation)
+        ) {
+            Text(text = "Tap me")
+        }
+    }
+
 }
 
 @Composable
